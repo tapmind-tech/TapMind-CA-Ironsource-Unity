a3aa032d663d1b9c5d9be375611038a493099c9a
Kishan Italiya <kishan.italiya@tuvoc.com>
2026-06-16 17:22:38 +0530

    Removed the singleton flow in order to return the perfect ads for each placements

--- Editor/PostProcessIOS.cs
+++ Editor/PostProcessIOS.cs
@@ -26,8 +26,8 @@ public class PostProcessIOS
             return;
 
         string tapMindPods =
-            "  pod 'TapMindISAdapter', '2.1.4'\n" +
-            "  pod 'TapMindSDK', '1.2.0'\n";
+            "  pod 'TapMindISAdapter', '2.1.10'\n" +
+            "  pod 'TapMindSDK', '1.2.7'\n";
 
         // Insert pods before `end` of UnityFramework target
         string targetBlock = "target 'UnityFramework' do";
